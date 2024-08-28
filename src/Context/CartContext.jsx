11e926b64edcf61cacd553e53fa464bfc25960a7
yosefbenzo <yosef.mohamed2312@gmail.com>
2024-08-28 07:06:48 +0300

    payment

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -2,118 +2,141 @@ import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
-
-
 export let cartContext = createContext();
 
 export function CartContextProvider(props) {
+  let headers = {
+    token: localStorage.getItem("userToken"),
+  };
 
+  const [cartId, setCartId] = useState(null);
+  const [numOfCartItems, setNumOfCartItems] = useState(0);
 
-    let headers = {
-        token: localStorage.getItem('userToken'),
-    }
+  async function addToCart(productId) {
+    return axios
+      .post(
+        `https://ecommerce.routemisr.com/api/v1/cart`,
+        {
+          productId: productId,
+        },
+        {
+          headers: headers,
+        }
+      )
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
+  async function getLoggedUserCart() {
+    return axios
+      .get(`https://ecommerce.routemisr.com/api/v1/cart`, { headers })
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
-    // ----------->> Cart <<----------- 
+  async function removeItemFromCart(productId) {
+    return axios
+      .delete(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`, {
+        headers,
+      })
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
-    const [cartId, setCartId] = useState(null);
-    const [numOfCartItems, setNumOfCartItems] = useState(0);
+  async function updateProductCount(productId, count) {
+    return axios
+      .put(
+        `https://ecommerce.routemisr.com/api/v1/cart/${productId}`,
+        {
+          count: count,
+        },
+        { headers }
+      )
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
-    async function addToCart(productId) {
-        return axios.post(`https://ecommerce.routemisr.com/api/v1/cart`,
-            {
-                productId: productId
-            },
-            {
-                headers: headers
-            }).then((res) => res).catch((error) => error);
-    }
+// payment//////////////////////////////////
 
-    async function getLoggedUserCart() {
-        return axios.get(`https://ecommerce.routemisr.com/api/v1/cart`, { headers })
-            .then((res) => res)
-            .catch((error) => error);
-    }
+  async function onlinePayment(cartId, shippingAddress) {
+    const url = `${window.location.protocol}//${window.location.host}`;
+    console.log(url);
+    return axios
+      .post(
+        `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}`,
+        {
+          shippingAddress: shippingAddress,
+        },
+        { headers }
+      )
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
-    async function removeItemFromCart(productId) {
-        return axios.delete(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`, { headers })
-            .then((res) => res)
-            .catch((error) => error);
-    }
+  async function cashPayment(cartId, shippingAddress) {
+    return axios
+      .post(
+        `https://ecommerce.routemisr.com/api/v1/orders/${cartId}`,
+        {
+          shippingAddress: shippingAddress,
+        },
+        { headers }
+      )
+      .then((res) => res)
+      .catch((error) => error);
+  }
+////////////////////////////////////////////////////////////////////////
+  async function removeCart() {
+    return axios
+      .delete(`https://ecommerce.routemisr.com/api/v1/cart`, { headers })
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
-    async function updateProductCount(productId, count) {
-        return axios.put(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`, {
-            count: count,
-        }, { headers })
-            .then((res) => res)
-            .catch((error) => error);
-    }
+  async function getLoggedUserOrders(userId) {
+    return axios
+      .get(`https://ecommerce.routemisr.com/api/v1/orders/user/${userId}`)
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
-    async function onlinePayment(cartId, shippingAddress) {
-        const url = `${window.location.protocol}//${window.location.host}`;
-        console.log(url);
-        return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=${url}`, {
-            shippingAddress: shippingAddress,
-        }, { headers })
-            .then((res) => res)
-            .catch((error) => error);
-    }
+  const [numOfFavoriteItems, setNumOfFavoriteItems] = useState(0);
+  const [wishListDetails, setWishListDetails] = useState([]);
 
-    async function cashPayment(cartId, shippingAddress) {
-        return axios.post(`https://ecommerce.routemisr.com/api/v1/orders/${cartId}`, {
-            shippingAddress: shippingAddress,
-        }, { headers })
-            .then((res) => res)
-            .catch((error) => error);
-    }
+  async function getLoggedWishList() {
+    return axios
+      .get(`https://ecommerce.routemisr.com/api/v1/wishlist`, { headers })
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
-    async function removeCart() {
-        return axios.delete(`https://ecommerce.routemisr.com/api/v1/cart`, { headers })
-            .then((res) => res)
-            .catch((error) => error);
-    }
+  async function addToWishList(productId) {
+    return axios
+      .post(
+        `https://ecommerce.routemisr.com/api/v1/wishlist`,
+        {
+          productId: productId,
+        },
+        {
+          headers: headers,
+        }
+      )
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
-    async function getLoggedUserOrders(userId) {
-        return axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${userId}`)
-            .then((res) => res)
-            .catch((error) => error);
-    }
+  async function removeItemFromWishList(productId) {
+    return axios
+      .delete(`https://ecommerce.routemisr.com/api/v1/wishlist/${productId}`, {
+        headers,
+      })
+      .then((res) => res)
+      .catch((error) => error);
+  }
 
-
-
-
-
-
-    // ----------->> Wish List <<-----------
-
-    const [numOfFavoriteItems, setNumOfFavoriteItems] = useState(0);
-    const [wishListDetails, setWishListDetails] = useState([]);
-
-    async function getLoggedWishList() {
-        return axios.get(`https://ecommerce.routemisr.com/api/v1/wishlist`, { headers })
-            .then((res) => res)
-            .catch((error) => error);
-    }
-
-    async function addToWishList(productId) {
-        return axios.post(`https://ecommerce.routemisr.com/api/v1/wishlist`,
-            {
-                productId: productId
-            },
-            {
-                headers: headers
-            }).then((res) => res).catch((error) => error);
-    }
-
-    async function removeItemFromWishList(productId) {
-        return axios.delete(`https://ecommerce.routemisr.com/api/v1/wishlist/${productId}`, { headers })
-            .then((res) => res)
-            .catch((error) => error);
-    }
-
-    
-
-    return <cartContext.Provider value={{
+  return (
+    <cartContext.Provider
+      value={{
         cartId,
         setCartId,
         numOfCartItems,
@@ -133,8 +156,10 @@ export function CartContextProvider(props) {
         getLoggedWishList,
         addToWishList,
         removeItemFromWishList,
-    }} >
-        {props.children}
+      }}
+    >
+      {props.children}
     </cartContext.Provider>
+  );
 }
-// BENZO  
+// BENZO
